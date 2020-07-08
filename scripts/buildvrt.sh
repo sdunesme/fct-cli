11347bccc67c5cca0b3f06cbd2a7702749a8c812
Christophe ROUSSON <c.rousson.gpk@gmail.com>
2020-07-08 02:42:39 +0200

    Tests Var

--- scripts/buildvrt.sh
+++ scripts/buildvrt.sh
@@ -7,17 +7,17 @@ find ACC -name "CESBIO_ACC_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 LANDCOVE
 
 # Per axis Datasets
 
-find TILES -name "FLOW_DIST_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 FLOW_DIST.vrt
-find TILES -name "FLOW_RELZ_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 FLOW_RELZ.vrt
-find TILES -name "AXIS_MEASURE_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 AXIS_MEASURE.vrt
-find TILES -name "AXIS_DISTANCE_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 AXIS_DISTANCE.vrt
-find TILES -name "DGO_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 DGO.vrt
-find TILES -name "NEAREST_RELZ_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 NEAREST_RELZ.vrt
-find TILES -name "NEAREST_DISTANCE_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 NEAREST_DISTANCE.vrt
+find 10K -name "FLOW_DISTANCE_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 FLOW_DISTANCE.vrt
+find 10K -name "FLOW_HEIGHT_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 FLOW_HEIGHT.vrt
+find 10K -name "AXIS_MEASURE_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 AXIS_MEASURE.vrt
+find 10K -name "AXIS_DISTANCE_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 AXIS_DISTANCE.vrt
+find 10K -name "DGO_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 DGO.vrt
+find 10K -name "NEAREST_HEIGHT_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 NEAREST_HEIGHT.vrt
+find 10K -name "NEAREST_DISTANCE_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 NEAREST_DISTANCE.vrt
 
-find TILES -name "CONTINUITY_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 CONTINUITY.vrt
+find 10K -name "LANDCOVER_CONTINUITY_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 LANDCOVER_CONTINUITY.vrt
 
-find TILES -name "BUFFER30_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 BUFFER30.vrt
-find TILES -name "BUFFER100_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 BUFFER100.vrt
-find TILES -name "BUFFER200_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 BUFFER200.vrt
-find TILES -name "BUFFER1000_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 BUFFER1000.vrt
+find 10k -name "BUFFER30_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 BUFFER30.vrt
+find 10k -name "BUFFER100_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 BUFFER100.vrt
+find 10k -name "BUFFER200_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 BUFFER200.vrt
+find 10k -name "BUFFER1000_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 BUFFER1000.vrt
