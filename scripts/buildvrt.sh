cd6374ca7243c7b186d3aaca37320acf65d1eca7
Christophe ROUSSON <c.rousson.gpk@gmail.com>
2020-07-22 23:36:13 +0200

    SubGrid Profile

--- scripts/buildvrt.sh
+++ scripts/buildvrt.sh
@@ -1,25 +1,43 @@
+#!/bin/bash
+
+function mkvrt {
+    SUBDIR=$1
+    PREFIX=$2
+    PATTERN="$PREFIX"_*.tif
+    find $SUBDIR -name "$PATTERN" |
+        xargs gdalbuildvrt -a_srs EPSG:2154 $PREFIX.vrt
+}
+
+# Copies from regional processing
+
+mkvrt 10K RGE5M
+mkvrt 10K FLOW_RGE5M
+mkvrt 10K ACC_RGE5M
+
 # Global Datasets
 
-find LANDCOVER -name "CESBIO_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 LANDCOVER_2018.vrt
-find POPULATION -name "POP_INSEE_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 POP_2015.vrt
-find ACC -name "POP_INSEE_ACC_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 POP_2015_ACC.vrt
-find ACC -name "CESBIO_ACC_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 LANDCOVER_2018_ACC.vrt
+mkvrt 10K LANDCOVER_2018 LANDCOVER_2018.vrt
+mkvrt 10K POP_2015
+# find ACC -name "POP_INSEE_ACC_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 POP_2015_ACC.vrt
+# find ACC -name "CESBIO_ACC_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 LANDCOVER_2018_ACC.vrt
 
 # Per axis Datasets
 
-find 10K -name "FLOW_DISTANCE_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 FLOW_DISTANCE.vrt
-find 10K -name "FLOW_HEIGHT_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 FLOW_HEIGHT.vrt
-find 10K -name "AXIS_MEASURE_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 AXIS_MEASURE.vrt
-find 10K -name "AXIS_DISTANCE_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 AXIS_DISTANCE.vrt
-find 10K -name "DGO_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 DGO.vrt
-find 10K -name "NEAREST_HEIGHT_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 NEAREST_HEIGHT.vrt
-find 10K -name "NEAREST_DISTANCE_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 NEAREST_DISTANCE.vrt
-find 10K -name "VALLEY_BOTTOM_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 VALLEY_BOTTOM.vrt
-find 10K -name "VALLEY_DISTANCE_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 VALLEY_DISTANCE.vrt
+mkvrt 10K FLOW_DISTANCE
+mkvrt 10K FLOW_HEIGHT
 
-find 10K -name "LANDCOVER_CONTINUITY_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 LANDCOVER_CONTINUITY.vrt
+mkvrt 10K NEAREST_HEIGHT
+mkvrt 10K NEAREST_DISTANCE
 
-find 10k -name "BUFFER30_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 BUFFER30.vrt
-find 10k -name "BUFFER100_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 BUFFER100.vrt
-find 10k -name "BUFFER200_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 BUFFER200.vrt
-find 10k -name "BUFFER1000_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 BUFFER1000.vrt
+mkvrt 10K AXIS_MEASURE
+mkvrt 10K AXIS_DISTANCE
+mkvrt 10K DGO
+
+mkvrt 10K VALLEY_BOTTOM
+mkvrt 10K VALLEY_DISTANCE
+
+mkvrt 10K LANDCOVER_CONTINUITY
+
+mkvrt 10k BUFFER_DISTANCE
+mkvrt 10K BUFFER_MASK
+mkvrt 10K SUBGRID_WATERSHED
