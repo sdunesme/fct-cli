ca14656cb304c27ff2b3c4592fb4dee91d27efe5
Christophe ROUSSON <c.rousson.gpk@gmail.com>
2020-07-23 18:13:37 +0200

    Code rearrange

--- scripts/buildvrt.sh
+++ scripts/buildvrt.sh
@@ -18,6 +18,7 @@ mkvrt 10K ACC_RGE5M
 
 mkvrt 10K LANDCOVER_2018 LANDCOVER_2018.vrt
 mkvrt 10K POP_2015
+mkvrt 10K SNV_2015
 # find ACC -name "POP_INSEE_ACC_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 POP_2015_ACC.vrt
 # find ACC -name "CESBIO_ACC_*.tif" | xargs gdalbuildvrt -a_srs EPSG:2154 LANDCOVER_2018_ACC.vrt
 
@@ -36,8 +37,11 @@ mkvrt 10K DGO
 mkvrt 10K VALLEY_BOTTOM
 mkvrt 10K VALLEY_DISTANCE
 
+mkvrt 10K LANDCOVER_CLASSES
 mkvrt 10K LANDCOVER_CONTINUITY
 
 mkvrt 10k BUFFER_DISTANCE
 mkvrt 10K BUFFER_MASK
+mkvrt 10K BUFFER_PROFILE
+
 mkvrt 10K SUBGRID_WATERSHED
